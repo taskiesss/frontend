975878d4049c343e66dd9eddc2774fb6715d7119
Andrew Basem <andrewbassemsh@gmail.com>
2025-06-18 17:21:26 +0300

    add second section home page

--- app/guest/home/page.tsx
+++ app/guest/home/page.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Button from "@/app/_components/common/button";
 import AnimatedWords from "./AnimatedWords";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
     <main
-      className="min-h-screen p-4 
+      className="min-h-screen py-7 
         bg-[var(--background-color)] 
         text-[var(--accent-color)]"
     >
-      <div className="flex flex-col gap-6 w-full mx-auto">
+      <div className="flex flex-col w-full mx-auto">
         {/* Section 1 */}
-        <section className="flex flex-col gap-48 justify-center p-6 relative rounded-lg shadow-md bg-cover bg-center min-h-[500px]">
+        <section className="flex flex-col gap-48 justify-center p-10 relative rounded-lg shadow-md bg-cover bg-center min-h-[500px]">
           {/* Background Image with Contrast and Opacity */}
           <div
             className="absolute inset-0 bg-cover bg-center opacity-20"
@@ -28,7 +29,7 @@ export default function HomePage() {
           ></div>
 
           {/* Content */}
-          <div className="relative z-10 flex flex-col md:flex-row items-center justify-between ">
+          <div className="relative z-10 flex flex-col md:flex-row items-center justify-between">
             <div className="md:w-1/2 flex flex-col gap-10">
               <AnimatedWords />
               <span className="text-3xl">
@@ -41,26 +42,96 @@ export default function HomePage() {
 
           {/* Button */}
           <div className="relative z-10 flex justify-center w-full mt-4">
-            <Button className="px-8 py-4 text-xl ">Get Started</Button>
+            <Button className="px-8 py-4 text-xl">Get Started</Button>
           </div>
         </section>
 
         {/* Section 2 */}
-        <section
-          className="flex-1 p-6 
-            rounded-lg shadow-md
-            bg-[var(--foreground-color)]"
-        >
-          <h2 className="text-xl font-bold mb-4">Section Two</h2>
-          <p>This is the second section. You can add more details here.</p>
+
+        <section className="flex-1 py-24  shadow-md bg-[var(--foreground-color)]">
+          <div className="p-6">
+            <h3 className="text-3xl font-bold text-center mb-20">
+              How to Hire a Freelancers through Taskaya
+            </h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+              {/* Card 1 */}
+              <div className="flex flex-col items-center text-center p-7 bg-[var(--background-color)] shadow-lg hover:shadow-xl transition-shadow duration-300 rounded-xl">
+                <Image
+                  src="/images/open.png"
+                  alt="Post Your Job Icon"
+                  width={80}
+                  height={80}
+                  className="mb-4"
+                />
+                <h4 className="text-xl font-semibold mb-7">
+                  Post Your Job (For Free!)
+                </h4>
+                <p className="text-sm text-[var(--accent-color)] opacity-80">
+                  Post a job and share with us your all your requirements. The
+                  more information you include, the easier you find the perfect
+                  freelancer for your job.
+                </p>
+              </div>
+
+              {/* Card 2 */}
+              <div className="flex flex-col items-center text-center p-7 bg-[var(--background-color)] shadow-lg hover:shadow-xl transition-shadow duration-300 rounded-xl">
+                <Image
+                  src="/images/message.png"
+                  alt="Filter Proposals Icon"
+                  width={80}
+                  height={80}
+                  className="mb-4"
+                />
+                <h4 className="text-xl font-semibold mb-7">
+                  Filter the Proposals
+                </h4>
+                <p className="text-sm text-[var(--accent-color)] opacity-80">
+                  Browse through freelancers’ proposals, profiles, and compare
+                  before choosing the right freelancer for the job.
+                </p>
+              </div>
+
+              {/* Card 3 */}
+              <div className="flex flex-col items-center text-center p-7 bg-[var(--background-color)] shadow-lg hover:shadow-xl transition-shadow duration-300 rounded-xl">
+                <Image
+                  src="/images/chat.png"
+                  alt="Interview Freelancers Icon"
+                  width={80}
+                  height={80}
+                  className="mb-4"
+                />
+                <h4 className="text-xl font-semibold mb-7">
+                  Interview Shortlisted Freelancers
+                </h4>
+                <p className="text-sm text-[var(--accent-color)] opacity-80">
+                  Use Taskaya’s chat box to select and talk with or interview
+                  your top freelancers and discuss the job details to pick your
+                  final freelancer and start right away.
+                </p>
+              </div>
+
+              {/* Card 4 */}
+              <div className="flex flex-col items-center text-center p-5 rounded-lg bg-[var(--background-color)] shadow-lg hover:shadow-xl transition-shadow duration-300 rounded-xl">
+                <Image
+                  src="/images/secure.png"
+                  alt="Pay Safely Icon"
+                  width={80}
+                  height={80}
+                  className="mb-4"
+                />
+                <h4 className="text-xl font-semibold mb-2">Pay Safely</h4>
+                <p className="text-sm text-[var(--accent-color)] opacity-80">
+                  With secure payments you’ll receive invoices through Taskaya.
+                  We hold the money until the project is completed, and you
+                  approve its release to the freelancer.
+                </p>
+              </div>
+            </div>
+          </div>
         </section>
 
         {/* Section 3 */}
-        <section
-          className="flex-1 p-6 
-            rounded-lg shadow-md
-            bg-[var(--foreground-color)]"
-        >
+        <section className="flex-1 p-6 rounded-lg shadow-md bg-[var(--foreground-color)]">
           <h2 className="text-xl font-bold mb-4">Section Three</h2>
           <p>
             The final section contains additional information or call-to-action.
